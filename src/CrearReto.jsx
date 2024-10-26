afb061c29f0206d748a87b01007c99df3141d8d2
Sebassssssssssss <81488575+SebastianOcampocr@users.noreply.github.com>
2024-10-26 01:17:15 -0600

    Update CrearReto.jsx

--- src/CrearReto.jsx
+++ src/CrearReto.jsx
@@ -17,7 +17,7 @@ const CrearReto = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:3000/crear-reto', {
+            const response = await fetch('https://timedata-ruddy.vercel.app/crear-reto', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
