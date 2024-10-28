44c82c4223deff3f12a07b81bc6e033f9e6ec780
Sebassssssssssss <81488575+SebastianOcampocr@users.noreply.github.com>
2024-10-27 20:48:57 -0600

    Update RegistroCoder.jsx

--- src/RegistroCoder.jsx
+++ src/RegistroCoder.jsx
@@ -19,7 +19,7 @@ const Registrocoder = () => {
             setError('');
             // Intentar registrarse
             try {
-                const response = await fetch('https://timedata-ruddy.vercel.app/register', {
+                const response = await fetch('/register', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
