c8bab4c495cb57c165c5523a7b34bf7c666227a9
Sebassssssssssss <81488575+SebastianOcampocr@users.noreply.github.com>
2024-10-27 19:45:30 -0600

    Update Chatbot.jsx

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,22 +1,40 @@
 import './Chatbot.css';
-
+import { useEffect } from 'react';
 
 const Chatbot = () => {
-  	return (
-    		<div className="chatbot">
-      			<div className="b16170a5347c77eee4653-welcome">
-        				<div className="b16170a5347c77eee4653-welcome1" />
-      			</div>
-      			<div className="parte-escribir">
-        				<img className="parte-escribir-child" alt="" src="Group 8.svg" />
-        				<div className="envia-un-mensaje-al-chat-bot-wrapper">
-          					<div className="envia-un-mensaje">Envia un mensaje al chat bot</div>
-        				</div>
-      			</div>
-      			<div className="cuadrotexto">
-        				<b className="calculadora-html-css">Calculadora html, css y js</b>
-      			</div>
-    		</div>);
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://cdn.voiceflow.com/widget/bundle.mjs';
+        script.onload = () => {
+            if (window.voiceflow && window.voiceflow.chat) {
+                window.voiceflow.chat.load({
+                    verify: { projectID: '671dc0af6e800cb63f25f821' },
+                    url: 'https://general-runtime.voiceflow.com/',
+                    versionID: 'production',
+                });
+            } else {
+                console.error("Voiceflow chat no está disponible.");
+            }
+        };
+        script.onerror = () => {
+            console.error("Error al cargar el script de Voiceflow.");
+        };
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div className="chatbot">
+            <div className="b16170a5347c77eee4653-welcome">
+                <div className="b16170a5347c77eee4653-welcome1" />
+            </div>
+        </div>
+    );
 };
 
 export default Chatbot;
+
